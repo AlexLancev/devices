993fc5fc9b1b1cb0ab8ba73dc3ac42d3bca192db
Aleksei Vavulo <92637953+AlexLancev@users.noreply.github.com>
2022-10-14 08:17:38 +0500

    Delete hidden-images.js

--- js/hidden-images.js
+++
@@ -1,7 +0,0 @@
-const containerShow = document.querySelectorAll('.container-show');
-
-containerShow.forEach(function(item) {
-    item.addEventListener('click', function() {
-        this.classList.add('container-show--active');
-    })
-})
