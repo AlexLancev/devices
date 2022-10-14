a8feb64b26ac5f2b5e30e0cacd26560a5cde435f
Aleksei Vavulo <92637953+AlexLancev@users.noreply.github.com>
2022-10-14 08:18:38 +0500

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,20 @@
+const headerBurger = document.querySelector('.header__burger');
+const navListContent = document.querySelector('.nav__list-device');
+const bodyFixed = document.querySelector('body');
+const navLink = document.querySelectorAll('.nav__link');
+
+headerBurger.addEventListener('click', function() {
+
+    this.classList.toggle('active');
+    navListContent.classList.toggle('active');
+    bodyFixed.classList.toggle('lock');
+
+})
+
+navLink.forEach(function(item) {
+    item.addEventListener('click', function() {
+        navListContent.classList.remove('active');
+        headerBurger.classList.remove('active');
+        bodyFixed.classList.remove('lock');
+    })
+  })
